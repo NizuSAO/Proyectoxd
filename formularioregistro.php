4267f2f6d4c95c46188b388ea5a2b4458c87e376
NizuSAO <nizusao@gmail.com>
2021-11-12 15:21:31 -0300

    hoy juega la scalonetta

---
+++ formularioregistro.php
@@ -0,0 +1,33 @@
+<?php
+	include("registrar.php") 
+	 ?>
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial scale=1.0">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="sao.css">
+	<title>Hardware Machine</title>
+</head>
+<body>
+	<form method="post">
+	<h1 style="color: white">ingresar datos</h1>
+	<section class="form-register">
+		<h3>Registro de jugador</h3>
+		<input class="controls" type="text" name="stock" placeholder="Ingrese si hay stock">
+		<input class="controls" type="text" name="precio" placeholder="ingresar su contraseña">
+		<input class="controls" type="text" name="idproducto" placeholder="ingresar la id del producto">
+		<input class="controls" type="text" name="nomproducto" placeholder="ingresar el nombre del producto">
+		<input class="controls" type="text" name="ventas" placeholder="ingresar la cantidad de ventas">
+		<input class="controls" type="text" name="ganancias" placeholder="ingresar las ganancias">
+		<p>Estoy de acuerdo con los <a href="terminosycondiciones.html">terminos y condiciones</a> </p>
+		<input class="botons" type="submit" name="Registrar"> 
+		<p><a href="yatengocuenta.html">ya tengo cuenta</a></p>
+	</section>
+	</form>
+
+	
+
+</body>
+</html>
