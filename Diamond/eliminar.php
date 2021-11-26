0c7a21d8f9f1e31f6fd21201ad94cb1952a6e140
cristian rios <cnrios2015@gmail.com>
2021-11-26 01:45:45 -0300

    eliminar

---
+++ Diamond/eliminar.php
@@ -0,0 +1,11 @@
+<?php/*
+	include "conexion.php";
+
+	eliminar($_GET['no']);
+
+	function eliminar($no)
+	{
+		$sentencia="DELETE FROM sucursal_1 WHERE no='".$no."' ";
+		mysqli_query($sentencia) or die (mysqli_error());
+	}
+*/?>
